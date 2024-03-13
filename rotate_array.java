88eab54cb4eb6fffb33f65044a67b1561a1f6707
kirannikam <nikamsarkar464@gmail.com>
2024-03-13 22:37:30 +0530

    Q) Rotate Array

--- rotate_array.java
+++ rotate_array.java
@@ -3,15 +3,13 @@
     Given an unsorted array arr[] of size N. Rotate the array to the left (counter-clockwise direction) by D steps, where D is a positive integer. 
 
     Example 1:
-    Input:
-    N = 5, D = 2
+    Input: N = 5, D = 2
     arr[] = {1,2,3,4,5}
     Output: 3 4 5 1 2
     Explanation: 1 2 3 4 5  when rotated by 2 elements, it becomes 3 4 5 1 2.
     
     Example 2:
-    Input:
-    N = 10, D = 3
+    Input: N = 10, D = 3
     arr[] = {2,4,6,8,10,12,14,16,18,20}
     Output: 8 10 12 14 16 18 20 2 4 6
     Explanation: 2 4 6 8 10 12 14 16 18 20 when rotated by 3 elements, it becomes 8 10 12 14 16 18 20 2 4 6.
@@ -40,6 +38,10 @@ public class rotate_array {
             arr[i] = a[i];
     }
     public static void main(String[] args) {
-        
+        int n = 5, d = 2;
+        int[] arr = {1,2,3,4,5};
+        rotateArr(arr, d, n);
+        for(int i=0; i<n; i++)
+            System.out.print(arr[i] + " ");
     }
 }
