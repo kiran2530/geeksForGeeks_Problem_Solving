c14f027cae19eeb9c0c428524a0a0499ad40c026
kirannikam <nikamsarkar464@gmail.com>
2024-08-19 21:39:32 +0530

    Q) Kth Smallest

--- Daily Questions/2024/08-2024/_19_08_24.java
+++ Daily Questions/2024/08-2024/_19_08_24.java
@@ -1,20 +1,22 @@
 /*
  *  Q) Kth Smallest
         Given an array arr[] and an integer k where k is smaller than the size of the array, the task is to find the kth smallest element in the given array. It is given that all array elements are distinct.
-
         Follow up: Don't solve it using the inbuilt sort function.
 
-        Examples :
-
+    Example : 1
         Input: arr[] = [7, 10, 4, 3, 20, 15], k = 3
         Output:  7
         Explanation: 3rd smallest element in the given array is 7.
+        
+    Example : 2
         Input: arr[] = [7, 10, 4, 20, 15], k = 4 
         Output:  15
         Explanation: 4th smallest element in the given array is 15.
-        Expected Time Complexity: O(n+(max_element) )
-        Expected Auxiliary Space: O(max_element)
-        Constraints:
+        
+    Expected Time Complexity: O(n+(max_element) )
+    Expected Auxiliary Space: O(max_element)
+        
+    Constraints:
         1 <= arr.size <= 10^6
         1<= arr[i] <= 10^6
         1 <= k <= n
